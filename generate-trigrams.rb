e054bb72209c0264140ea798dff7d493db41fb4d
Eric Petersen <eric@huskers.unl.edu>
2016-01-09 21:01:15 -0800

    Skip characters with fewer than 10 lines; sub "_" for "/"

--- generate-trigrams.rb
+++ generate-trigrams.rb
@@ -6,6 +6,7 @@ require 'pry'
 require 'uri'
 Dotenv.load
 
+MIN_SENTENCES = 10
 
 dialogue = get_dialogue
 
@@ -19,17 +20,24 @@ response = client.class.get("/data/#{trigram_collection}")
 JSON.parse(response.body)["files"].each do |file|
   existing_files.push(file["filename"])
 end
+puts existing_files
 
 # the key is a character name
 # the value is an array of all their lines of dialogue
 dialogue.each do |character, paragraphs|
-  trigram_file_name = "#{URI.encode(character)}.txt"
+  trigram_file_name = "#{character.sub('/', '_')}.txt"
 
   if existing_files.include? trigram_file_name 
     puts "Already have trigrams for #{character}"
     next
   else
-    puts "Generating trigrams for #{character}..."
+    if paragraphs.length < MIN_SENTENCES
+      # skip characters with very few lines
+      puts "Skipping #{character}: only #{paragraphs.length} lines"
+      next
+    else
+      puts "Generating trigrams for #{character}..."
+    end
   end
 
   # generate array of sentences
@@ -43,7 +51,7 @@ dialogue.each do |character, paragraphs|
   input = [
     corpus,
     "xxBeGiN142xx", "xxEnD142xx",
-    "data://#{trigram_collection}/#{trigram_file_name}",
+    "data://#{trigram_collection}/#{URI.encode(trigram_file_name)}",
   ]
   response = client.class.post(
     "/algo/ngram/GenerateTrigramFrequencies/0.1.1",
